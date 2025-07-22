7918ec5ebca31ac473c5080ecd9d1fb4e3dd9eff
Yuz <wwowkdrr@gmail.com>
2025-07-22 16:33:25 +0800

    建立預告片動畫css及存取預告片圖片

--- front/main.php
+++ front/main.php
@@ -1,67 +1,101 @@
-      <style>
-        .lists {
-          width: 210px;
-          height: 240px;
-          background: lightblue;
-          margin: auto;
-        }
+    <style>
+      .lists {
+        width: 210px;
+        height: 240px;
+        margin: 0 auto;
+        position: relative;
+        overflow: hidden;
+      }
 
-        .btns {
-          width: 320px;
-          height: 120px;
-          background: lightpink;
-          margin: 0 auto;
-        }
+      .btns {
+        width: 320px;
+        height: 120px;
 
-        .left, .right{
-          width: 0;
-          height: 0;
-          
-          border-top: 20px solid transparent;
-          border-bottom: 20px solid transparent;
-        }
+      }
 
-        .left {
-          border-left: 0px solid black;
-          border-right: 30px solid black;
-        }
+      .left,
+      .right {
+        width: 0;
+        height: 0;
+        border-top: 20px solid transparent;
+        border-bottom: 20px solid transparent;
 
-        .right {
-          border-left: 30px solid black;
-          border-right: 0px solid black;
-        }
+      }
 
-        .controls{
-          display: flex;
-          justify-content: space-around;
-          align-items: center;
-        }
-      </style>
+      .left {
+        border-left: 0px solid black;
+        border-right: 30px solid black;
 
+      }
 
+      .right {
+        border-left: 30px solid black;
+        border-right: 0px solid black;
 
-      <div class="half" style="vertical-align:top;">
-        <h1>預告片介紹</h1>
-        <div class="rb tab" style="width:95%;">
-          <div>
-            <div class="lists">
-            </div>
-            <div class="controls">
-              <div class="left"></div>
-              <div class="btns"></div>
-              <div class="right"></div>
-            </div>
+      }
+
+      .controls {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+      }
+
+      .poster {
+        text-align: center;
+        position: absolute;
+        width: 210px;
+        height: 240px;
+        display: none;
+      }
+
+      .poster img {
+        width: 200px;
+        height: 220px;
+
+      }
+    </style>
+    <?php
+    $posters = $Poster->all(['sh' => 1], " order by `rank`");
+
+    ?>
+    <div class="half" style="vertical-align:top;">
+      <h1>預告片介紹</h1>
+      <div class="rb tab" style="width:95%;">
+        <div>
+          <div class="lists">
+            <?php
+            foreach ($posters as $poster):
+            ?>
+              <div class="poster">
+                <img src="./image/<?= $poster['img']; ?>">
+                <div><?= $poster['name']; ?></div>
+              </div>
+            <?php
+            endforeach;
+            ?>
+          </div>
+
+          <div class="controls">
+            <div class="left"></div>
+            <div class="btns"></div>
+            <div class="right"></div>
           </div>
         </div>
       </div>
-      <div class="half">
-        <h1>院線片清單</h1>
-        <div class="rb tab" style="width:95%;">
-          <table>
-            <tbody>
-              <tr> </tr>
-            </tbody>
-          </table>
-          <div class="ct"> </div>
-        </div>
+    </div>
+    <script>
+      $(".poster").eq(0).show()
+    </script>
+
+
+    <div class="half">
+      <h1>院線片清單</h1>
+      <div class="rb tab" style="width:95%;">
+        <table>
+          <tbody>
+            <tr> </tr>
+          </tbody>
+        </table>
+        <div class="ct"> </div>
       </div>
+    </div>
