d105e1590c8fc92aadd76581e70c65f9609a9392
Yuz <wwowkdrr@gmail.com>
2025-07-22 14:20:59 +0800

    預告片編輯

--- back/poster.php
+++ back/poster.php
@@ -14,25 +14,26 @@
             ?>
                 <div class='ct' style="padding: 3px; display:flex; justify-content:space-between; align-items:center; background:white; margin-bottom:3px;">
                     <div style='width:24.8%'>
-                        <img src="./image/<?=$poster['img']?>" style="width:60px; height:80px;">
+                        <img src="./image/<?= $poster['img'] ?>" style="width:60px; height:80px;">
                     </div>
                     <div style='width:24.8%'>
-                        <input type="text" name="name[]" value="<?= $poster['name']?>" style="width:90%;">
+                        <input type="text" name="name[]" value="<?= $poster['name'] ?>" style="width:90%;">
                     </div>
                     <div style='width:24.8%'>
                         <button type='button'>往上</button>
                         <button type='button'>往下</button>
                     </div>
                     <div style='width:24.8%'>
-                        <input type="checkbox" name="sh[]" value="<?= $poster['id'];?>"<?= ($poster['sh'] == 1) ? 'checked':'';?>>顯示
-                        <input type="checkbox" name="del[]" value="<?= $poster['id'];?>">刪除
+                        <input type="checkbox" name="sh[]" value="<?= $poster['id']; ?>" <?= ($poster['sh'] == 1) ? 'checked' : ''; ?>>顯示
+                        <input type="checkbox" name="del[]" value="<?= $poster['id']; ?>">刪除
                         <select name="ani[]">
-                            <option value="1" <?=($poster['ani']==1)?'selected':'';?>> 淡入淡出</option>
-                            <option value="2" <?=($poster['ani']==2)?'selected':'';?>> 縮放</option>
-                            <option value="3" <?=($poster['ani']==3)?'selected':'';?>> 滑入滑出</option>
+                            <option value="1" <?= ($poster['ani'] == 1) ? 'selected' : ''; ?>> 淡入淡出</option>
+                            <option value="2" <?= ($poster['ani'] == 2) ? 'selected' : ''; ?>> 縮放</option>
+                            <option value="3" <?= ($poster['ani'] == 3) ? 'selected' : ''; ?>> 滑入滑出</option>
                         </select>
                     </div>
                 </div>
+                <input type="hidden" name="id[]" value="<?= $poster['id']; ?>">
             <?php
             endforeach;
             ?>
