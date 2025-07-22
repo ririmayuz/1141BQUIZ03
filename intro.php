796d3beba16b44e14e1631c0f2e72d92091fcf18
Yuz <wwowkdrr@gmail.com>
2025-07-22 09:25:13 +0800

    素材跟資料夾整理

--- intro.php
+++ intro.php
@@ -4,9 +4,10 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>影城</title>
-<link rel="stylesheet" href="css/css.css">
-<link href="Profile page_files/s2.css" rel="stylesheet" type="text/css">
-<script src="scripts/jquery-1.9.1.min.js"></script>
+  <link rel="stylesheet" href="css/css.css">
+  <link href="css/s2.css" rel="stylesheet" type="text/css">
+  <script src="js/jquery-1.9.1.min.js"></script>
+</head>
 </head>
 
 <body>
@@ -26,7 +27,7 @@
         <video src="movie/03B20v.avi" width="300px" height="250px" controls="" style="float:right;"></video>
         <font style="font-size:24px"> <img src="Profile page_files/03B20.png" width="200px" height="250px" style="margin:10px; float:left">
         <p style="margin:3px">影片名稱 ：
-          <input type="button" value="線上訂票" onclick="lof(&#39;?do=ord&amp;id=4&#39;)" style="margin-left:50px; padding:2px 4px" class="b2_btu">
+          <input type="button" value="線上訂票" onclick="lof('?do=ord&amp;id=4')" style="margin-left:50px; padding:2px 4px" class="b2_btu">
         </p>
         <p style="margin:3px">影片分級 ： <img src="Profile page_files/03C04.png" style="display:inline-block;">限制級 </p>
         <p style="margin:3px">影片片長 ： 時/分</p>
