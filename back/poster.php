9d5cd470bb61f76b3cc3ed705806e73b0b4f5d8c
Yuz <wwowkdrr@gmail.com>
2025-07-22 14:41:34 +0800

    在往上往下按鈕中加入要交換的資料id

--- back/poster.php
+++ back/poster.php
@@ -1,35 +1,39 @@
 <div style="height:300px;">
     <h3 class="ct" style="margin:0;">預告片清單</h3>
-    <div class='ct' style="display:flex; justify-content:space-between;">
+    <div class='ct' style="display:flex;justify-content:space-between;">
         <div style='width:24.8%;background:#ccc'>預告片海報</div>
         <div style='width:24.8%;background:#ccc'>預告片片名</div>
         <div style='width:24.8%;background:#ccc'>預告片排序</div>
         <div style='width:24.8%;background:#ccc'>操作</div>
     </div>
     <form action="./api/edit_poster.php" method="post">
-        <div style="overflow:auto; height:200px;">
+
+        <div style="overflow:auto;height:200px;">
             <?php
             $posters = $Poster->all(" order by `rank`");
-            foreach ($posters as $poster):
+            foreach ($posters as $idx => $poster):
+                $prev = ($idx - 1 >= 0) ? $posters[$idx - 1]['id'] : $poster['id'];
+                $next = ($idx + 1 < count($posters)) ? $posters[$idx + 1]['id'] : $poster['id'];
+
             ?>
-                <div class='ct' style="padding: 3px; display:flex; justify-content:space-between; align-items:center; background:white; margin-bottom:3px;">
+                <div class='ct' style="padding:3px;display:flex;justify-content:space-between;align-items:center;background:white;margin-bottom:3px;">
                     <div style='width:24.8%'>
-                        <img src="./image/<?= $poster['img'] ?>" style="width:60px; height:80px;">
+                        <img src="./image/<?= $poster['img'] ?>" style="width:60px;height:80px;">
                     </div>
                     <div style='width:24.8%'>
                         <input type="text" name="name[]" value="<?= $poster['name'] ?>" style="width:90%;">
                     </div>
                     <div style='width:24.8%'>
-                        <button type='button'>往上</button>
-                        <button type='button'>往下</button>
+                        <button type='button' data-sw='<?= $prev; ?>' data-id="<?= $poster['id']; ?>">往上</button>
+                        <button type='button' data-sw='<?= $next; ?>' data-id="<?= $poster['id']; ?>">往下</button>
                     </div>
                     <div style='width:24.8%'>
                         <input type="checkbox" name="sh[]" value="<?= $poster['id']; ?>" <?= ($poster['sh'] == 1) ? 'checked' : ''; ?>>顯示
                         <input type="checkbox" name="del[]" value="<?= $poster['id']; ?>">刪除
                         <select name="ani[]">
-                            <option value="1" <?= ($poster['ani'] == 1) ? 'selected' : ''; ?>> 淡入淡出</option>
-                            <option value="2" <?= ($poster['ani'] == 2) ? 'selected' : ''; ?>> 縮放</option>
-                            <option value="3" <?= ($poster['ani'] == 3) ? 'selected' : ''; ?>> 滑入滑出</option>
+                            <option value="1" <?= ($poster['ani'] == 1) ? 'selected' : ''; ?>>淡入淡出</option>
+                            <option value="2" <?= ($poster['ani'] == 2) ? 'selected' : ''; ?>>縮放</option>
+                            <option value="3" <?= ($poster['ani'] == 3) ? 'selected' : ''; ?>>滑入滑出</option>
                         </select>
                     </div>
                 </div>
