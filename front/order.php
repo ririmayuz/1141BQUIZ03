a1ef015e0a1b3e6cecbcfbc934910a4028438f81
Yuz <wwowkdrr@gmail.com>
2025-07-28 14:12:09 +0800

    線上訂票取得資料

--- front/order.php
+++ front/order.php
@@ -1,7 +1,7 @@
 <h3 class="ct">線上訂票</h3>
 <style>
     #orderForm {
-        width: 60%;
+        width: 50%;
         margin: 10px auto;
         padding: 20px;
         border: 1px solid #ccc;
@@ -19,7 +19,7 @@
 
     #orderForm td:nth-child(2) select{
         width: 98%;
-        text-align: right;
+        text-align: left;
     }
 </style>
 <table id='orderForm'>
@@ -40,3 +40,19 @@
     <button>確定</button>
     <button>重置</button>
 </div>
+
+<script>
+let url =new URLSearchParams(location.search);
+getMovies();
+
+function getMovies(){
+    let id = 0;
+    if(url.has('id')){
+        id=url.get('id');
+    }
+
+    $.get("./api/get_movies.php",{id},(movies)=>{
+        $("#movie").html(movies)
+    })
+}
+</script>
