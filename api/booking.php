4763e444f8b9b7ede71b13af0cb370c3a1007310
Yuz <wwowkdrr@gmail.com>
2025-08-01 14:52:54 +0800

    修正sprintf裡面用錯符號

--- api/booking.php
+++ api/booking.php
@@ -5,7 +5,7 @@ $_POST['tickets']=count($_POST['seats']);
 $_POST['seats']=serialize($_POST['seats']);
 $_POST['no']=date("Ymd");
 $maxNo=$Order->max("id")+1;
-$_POST['no'] .= sprintf("$04d",$maxNo);
+$_POST['no'] .= sprintf("%04d",$maxNo);
 
 $Order->save($_POST);
 
