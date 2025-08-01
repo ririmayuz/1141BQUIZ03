9dd2b9be83b035cc95394627ed5f5ca9d9876aee
Yuz <wwowkdrr@gmail.com>
2025-08-01 14:47:36 +0800

    完成訂購結果畫面

--- front/result-tc.php
+++ front/result-tc.php
@@ -1,20 +1,18 @@
-<?php
+<?php 
 $order=$Order->find(['no'=>$_GET['no']]);
 
 ?>
 <style>
     .result-table{
-        width: 540px;
-        margin: 20px auto;
-        /* border-collapse: collapse; */
-        padding: 20px;
-        border: 1px solid #ccc;
-        background-color: #eee;
-
+        width:540px;
+        margin:20px auto;
+        padding:20px;
+        border:1px solid #ccc;
+        background:#eee;
     }
-    .result-table td {
-        padding: 5px 2px;
-        border: 1px solid #ccc;
+    .result-table td{
+        padding:5px 2px;
+        border:1px solid #ccc;
     }
 
     .result-table tr:nth-child(even) td:nth-child(1){
@@ -41,7 +39,7 @@ $order=$Order->find(['no'=>$_GET['no']]);
     <tr>
         <td colspan="2">
             <div>座位：</div>
-            <?php
+            <?php 
             $seats=unserialize($order['seats']);
             sort($seats);
             foreach($seats as $seat){
@@ -49,14 +47,16 @@ $order=$Order->find(['no'=>$_GET['no']]);
                 echo floor($seat/5)+1 . "排" . ($seat%5)+1 . "號";
                 echo "</div>";
             }
-            
+
             ?>
-            <div>共<?=$order['tickets'];?>張電影片</div>
+
+            <div>共<?=$order['tickets'];?>張電影票</div>
         </td>
     </tr>
     <tr>
-        <td colspan="2">
+        <td colspan="2" class='ct'>
             <button onclick="location.href='index.php'">確認</button>
         </td>
+
     </tr>
 </table>
